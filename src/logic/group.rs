bf0846d65e319863d8388456ece4346d1f36b794
Faris Sufyan Suhaimi <sufyan.suhaimi@grabtaxi.com>
2024-01-06 01:40:22 +0800

    (docs) add task docs

--- src/logic/group.rs
+++ src/logic/group.rs
@@ -11,20 +11,20 @@ use sqlx::{self, Pool, Postgres, Transaction};
 use super::ledger::{self};
 
 pub trait GroupsHandler {
-    fn create_group(
+    pub fn create_group(
         &self,
         name: String,
         owner: AuthUser,
     ) -> impl std::future::Future<Output = Result<Group, Error>> + Send;
 
-    fn add_user_to_group(
+    pub fn add_user_to_group(
         &self,
         user: &AuthUser,
         group: &Group,
         tx: Option<&mut Transaction<'_, Postgres>>,
     ) -> impl std::future::Future<Output = Result<uuid::Uuid, Error>> + Send;
 
-    fn get_users_by_group(
+    pub fn get_users_by_group(
         &self,
         group_id: &uuid::Uuid,
         tx: Option<&mut Transaction<'_, Postgres>>,
